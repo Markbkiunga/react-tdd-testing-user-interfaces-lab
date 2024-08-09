8dec682f26a9a703d81cefe8cff985afa3a7c4df
Mark Brian <markbkiunga2017@gmail.com>
2024-08-09 14:06:15 +0300

    Completes Lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Mark</h1>
+      <img
+        src="https://avatars.githubusercontent.com/u/121306715?v=4"
+        alt="Mark Brian"
+        title="Mark Brian"
+      />
+      <h2>About Me</h2>
+      <p>My biography</p>
+      <a href="https://github.com/Markbkiunga">Github</a>
+      <br />
+      <a href="https://www.linkedin.com/in/markbriankiungakamau">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
